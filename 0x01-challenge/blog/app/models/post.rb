e885a07396c76ae21dadd89b85b6868ffdb081d1
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-12 13:38:50 -0700

    add attribute online for hide or showing the posts or hide it

--- 0x01-challenge/blog/app/models/post.rb
+++ 0x01-challenge/blog/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many  :comments, dependent: :destroy
+  attribute :online, :boolean, default: true
   validates :title, presence: true, length: {minimum: 5}
-	validates :body,  presence: true
+  validates :body,  presence: true
 end
--- 0x01-challenge/blog/db/schema.rb
+++ 0x01-challenge/blog/db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170124184440) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.boolean  "online"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
